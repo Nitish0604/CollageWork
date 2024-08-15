5c6606c2cdb3c1fb0cecbf340dc83ae8a6ce3142
Nitish kumar <139990315+Nitish0604@users.noreply.github.com>
2024-08-16 04:07:50 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 
 import './App.css';
 import Home from './pages/Home';
-import Signup from './pages/Signup';
+import Signup from './pages/Auth/Signup';
 import { Route, Routes} from 'react-router-dom';
 function App() {
   return (
     <div className="App">
-      {/* <div className='bg-red-500'>
-        Nitish
-      </div> */}
+     
       <Routes>
-        <Route path='/' element={<Home/>}></Route>
+        <Route path='/' element={<Signup/>}></Route>
         <Route path='/signup' element={<Signup/>}></Route>
       </Routes>
     </div>
